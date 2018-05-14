a820c432026974b83a97bee7df32fe2a593716e9
deboradom <deboradom@gmail.com>
2018-05-14 16:27:09 -0300

    minimal changes

--- todo-list-01/main.js
+++ todo-list-01/main.js
@@ -14,8 +14,9 @@ function addNovoItem(){
 
             li.innerHTML = valor
             item.appendChild(li)         
-            document.querySelector("input").value = "" // Não posso substituir essa linha por "valor", porque eu quero chamar de string vazio o resultado referente ao queryselector.value 
 
+            // Para limpar o input depois que adicionar cada tarefa:
+            document.querySelector("input").value = "" // Não posso substituir essa linha por "valor", porque eu quero chamar de string vazio o resultado referente ao queryselector.value 
 
         }
 }
@@ -23,7 +24,19 @@ function addNovoItem(){
 // Funcao que adiciona o traço de sobreescrito nas tarefas clicadas. 
 function addRisco(x){
     // console.log(event) // Esse console.log serve pra mostrar qual evento chama a função addRisco. Ele vai criar um MouseEvent no console, devo explorar até achar o atributo "target", e verificar se ele está relacionado à tag certa.
-    event.target.classList.add("done")
+    // event.target.classList.add("done")
+    
+    // Se quiser adicionar o taçado sem que possa ser retirado com outro clique sobre a tarefa:
+    // this.classList.add("done") // o 'this' é a mesma coisa que o 'event.target', ele retorno o mínimo escopo
+
+    // Se quiser adicionar o taçado de tarefa pronta ou removê-lo, com cliques.
+    if(this.classList.contains("done")){
+        this.classList.remove("done")
+    }else{
+        this.classList.add("done")
+    }
+
+
 }
 
 Outro 
